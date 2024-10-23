cf288e4d7b79dc00f39501fc863339066eaa79e9
hKriszti-svg <krisztinahorgos@yahoo.com>
2024-10-23 15:10:17 +0200

    edited js

--- src/index.js
+++ src/index.js
@@ -1,34 +1,27 @@
-function refreshWeather (response) {
+
+function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
-    let cityElement =document.querySelector("city");
-
+    let cityElement = document.querySelector("#city");
+  
     cityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML = Math.round(temperature);
-    
-
-}
-
-
-
-function searchCity(city){
-    let apiKey="ae026d6933ff4e9b69c0f323co1d1d9t";
-    let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  }
+  
+  function searchCity(city) {
+    let apiKey = "b2a5adcct04b33178913oc335f405433";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
     axios.get(apiUrl).then(refreshWeather);
-}
-
-
-
-
-function handleSearchSubmit (event){
+  }
+  
+  function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-   
+  
     searchCity(searchInput.value);
-
-}
-
-
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
-
+  }
+  
+  let searchFormElement = document.querySelector("#search-form");
+  searchFormElement.addEventListener("submit", handleSearchSubmit);
+  
+  searchCity("Berlin");
