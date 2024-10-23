173b98d406b6bbd4991bae108740bd4943b47eef
hKriszti-svg <krisztinahorgos@yahoo.com>
2024-10-23 15:06:45 +0200

    added js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
+function refreshWeather (response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement =document.querySelector("city");
+
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+    
+
+}
+
+
+
+function searchCity(city){
+    let apiKey="ae026d6933ff4e9b69c0f323co1d1d9t";
+    let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(refreshWeather);
+}
+
+
+
+
 function handleSearchSubmit (event){
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement=document.querySelector("#city");
-    cityElement.innerHTML=searchInput.value;
+   
+    searchCity(searchInput.value);
 
 }
 
